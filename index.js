c28a0efdd8c9de7f5910c323bc7c6a30fa61ba1a
Joseph <borkowski.joseph@gmail.com>
2023-07-15 13:16:13 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'JSKIIIII is the champion';
